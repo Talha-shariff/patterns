1f21753fa0d08093e22eb144dcfc1df4099ca617
Talha-shariff <sharifftalha004@gmail.com>
2025-06-17 11:30:18 +0530

    second pattern

--- Patt1.java
+++ Patt1.java
@@ -7,11 +7,19 @@ class FirstPattern{
             System.out.println();
         }
     }
+    public void pat2(){
+        for(int i=0; i<5; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
 }
 public class Patt1{
     public static void main(String[] args) {
         FirstPattern obj = new FirstPattern();
          obj.pat();
-        
+         obj.pat2();
     }
 }
