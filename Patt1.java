493a41c9cdfbd41a145f7fbde829e0a00f5a7adb
Talha-shariff <sharifftalha004@gmail.com>
2025-06-17 12:02:07 +0530

    fifth pattern

--- Patt1.java
+++ Patt1.java
@@ -31,13 +31,22 @@ class FirstPattern{
             System.out.println();
         }
     }
+    public void pat5(){
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<5-i+1; j++){
+                System.out.print( " * ");
+            }
+            System.out.println();
+        }
+    }
 }
 public class Patt1{
     public static void main(String[] args) {
         FirstPattern obj = new FirstPattern();
-         obj.pat();
-         obj.pat2();
-         obj.pat3();
-         obj.pat4();
+        //  obj.pat();
+        //  obj.pat2();
+        //  obj.pat3();
+        //  obj.pat4();
+         obj.pat5();
     }
 }
