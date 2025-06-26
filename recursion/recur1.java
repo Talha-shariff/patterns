b555a6b47370b224e2fd0e206bb7448511b08ba1
Talha-shariff <sharifftalha004@gmail.com>
2025-06-26 22:38:41 +0530

    printing name n times

--- recursion/recur1.java
+++ recursion/recur1.java
@@ -1,3 +1,16 @@
+// import java.util.Scanner;
+ 
 public class recur1 {
-    
+     static  void   print(int i,int n){
+        if(i>n)  return ;
+        System.out.println("Mohammed Talha Shariff ");
+          print(i+1, n); 
+     }
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // System.out.println(n);
+        // int n = 7;
+        print(1,7); 
+    }
 }
