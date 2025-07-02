c07667c6bbd8c32ab70142eb8819211401b57255
Talha-shariff <sharifftalha004@gmail.com>
2025-07-03 00:39:07 +0530

    calculating the factorial using functional recursion

---
+++ recursion/Fact.java
@@ -0,0 +1,14 @@
+public class Fact {
+    static double  fact(int n){
+        if (n==0){
+// at last the function will have the 0 value where 0! =  1 and if you are trying to return the 0 then anything multiplied with 0 =0
+            return 1;
+        }
+
+return n*fact(n-1);
+    }
+    public static void main(String[] args) {
+        int n =9;
+        System.out.print(fact(n));
+    }
+}
