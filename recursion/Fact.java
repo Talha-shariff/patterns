d718a690a19ce636a5abadf0a555e9da4b8d7906
Talha-shariff <sharifftalha004@gmail.com>
2025-07-05 16:23:23 +0530

    updated the function

--- recursion/Fact.java
+++ recursion/Fact.java
@@ -1,5 +1,5 @@
 public class Fact {
-    static double  fact(int n){
+    static double  factorial(int n){
         if (n==0){
 // at last the function will have the 0 value where 0! =  1 and if you are trying to return the 0 then anything multiplied with 0 =0
             return 1;
@@ -9,6 +9,6 @@ return n*fact(n-1);
     }
     public static void main(String[] args) {
         int n =9;
-        System.out.print(fact(n));
+        System.out.print(factorial(n));
     }
 }
