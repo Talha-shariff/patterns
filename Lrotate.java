735deb9cc8566d496bd72d100121a88c37a109c9
Talha-shariff <sharifftalha004@gmail.com>
2025-08-14 23:42:41 +0530

    rotates array element by left

--- Lrotate.java
+++ Lrotate.java
@@ -1,6 +1,6 @@
 
 class Lrotate{
-static void solve(int arr[], int n) {
+static void solveArray(int arr[], int n) {
   int temp = arr[0]; // storing the first element of array in a variable
   for (int i = 0; i < n - 1; i++) {
     arr[i] = arr[i + 1];
@@ -16,6 +16,6 @@ public static void main(String args[]) {
   int n=5;
   
   int arr[]= {1,2,3,4,5};
-  solve(arr, n);
+  solveArray(arr, n);
 }
 }
